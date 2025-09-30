fa5b10dd78186ee0fb64cd6f93ccf88d3b5492a5
Franciszek Saja <janusvomen1@gmail.com>
2025-09-30 16:48:20 +0200

    Update site URL to mw-site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwind from '@tailwindcss/vite'
 
 // Uzupełnij 'site' docelowym adresem produkcyjnym (wymagane m.in. dla sitemap)
 export default defineConfig({
-  site: 'https://FranekSaja.github.io/Maciej-Witek-Site',
-  base: '/Maciej-Witek-Site',
+  site: 'https://FranekSaja.github.io/mw-site',
+  base: '/mw-site',
   output: 'static',
   integrations: [mdx(), sitemap()],
   vite: { plugins: [tailwind()] },
