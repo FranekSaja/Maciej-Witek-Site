dfc926706f4981f210615ee5962bb7341b9a41b3
Franciszek Saja <janusvomen1@gmail.com>
2025-10-14 21:25:11 +0200

    feat: add author to publications and adjust styles

--- src/content.config.ts
+++ src/content.config.ts
@@ -59,6 +59,7 @@ const publications = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
+    author: z.string(),
     description: z.string().optional(),
     date: z.string().optional(),
     summary: z.string().optional(),
